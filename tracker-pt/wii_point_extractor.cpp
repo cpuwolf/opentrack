a8d849b30feda0b61058bdc944e54995b72c6339
Wei Shuai <cpuwolf@gmail.com>
2018-01-16 11:39:43 +0800

    wii_camera outputs wii raw data, wii_extractor create preview

--- tracker-pt/wii_point_extractor.cpp
+++ tracker-pt/wii_point_extractor.cpp
@@ -40,39 +40,87 @@ WIIPointExtractor::WIIPointExtractor(const QString& module_name) : s(module_name
 
 }
 
+//define a temp draw function
+void WIIPointExtractor::_draw_point(cv::Mat& preview_frame, const vec2& p, const cv::Scalar& color, int thinkness = 1)
+{
+	static constexpr int len = 9;
+
+	cv::Point p2(iround(p[0] * preview_frame.cols + preview_frame.cols / 2),
+		iround(-p[1] * preview_frame.cols + preview_frame.rows / 2));
+
+	cv::line(preview_frame,
+		cv::Point(p2.x - len, p2.y),
+		cv::Point(p2.x + len, p2.y),
+		color,
+		thinkness);
+	cv::line(preview_frame,
+		cv::Point(p2.x, p2.y - len),
+		cv::Point(p2.x, p2.y + len),
+		color,
+		thinkness);
+};
+
+bool WIIPointExtractor::_draw_points(cv::Mat& preview_frame, struct wii_info &wii, std::vector<vec2>& points)
+{
+	points.reserve(4);
+	points.clear();
+
+	for (unsigned index = 0; index < 4; index++)
+	{
+		struct wii_info_points &dot = wii.Points[index];
+		if (dot.bvis) {
+			//qDebug() << "wii:" << dot.RawX << "+" << dot.RawY;
+
+			const float W = 1024.0f;
+			const float H = 768.0f;
+			const float RX = W - dot.ux;
+			const float RY = H - dot.uy;
+			//vec2 dt((dot.RawX - W / 2.0f) / W, -(dot.RawY - H / 2.0f) / W);
+			//anti-clockwise rotate 2D point
+			vec2 dt((RX - W / 2.0f) / W, -(RY - H / 2.0f) / W);
+
+			points.push_back(dt);
+			_draw_point(preview_frame, dt, cv::Scalar(0, 255, 0), dot.isize);
+		}
+	}
+	const bool success = points.size() >= PointModel::N_POINTS;
+
+	return success;
+}
+
+void WIIPointExtractor::_draw_bg(cv::Mat& preview_frame, struct wii_info &wii)
+{
+	//draw battery status
+	cv::line(preview_frame,
+		cv::Point(0, 0),
+		cv::Point(preview_frame.cols*wii.BatteryPercent / 100, 0),
+		(wii.bBatteryDrained ? cv::Scalar(255, 0, 0) : cv::Scalar(0, 140, 0)),
+		2);
+
+	//draw horizon
+	int pdelta = iround((preview_frame.rows / 2) * tan((wii.Pitch)* M_PI / 180.0f));
+	int rdelta = iround((preview_frame.cols / 4) * tan((wii.Roll)* M_PI / 180.0f));
+
+	cv::line(preview_frame,
+		cv::Point(0, preview_frame.rows / 2 + rdelta - pdelta),
+		cv::Point(preview_frame.cols, preview_frame.rows / 2 - rdelta - pdelta),
+		cv::Scalar(80, 80, 80),
+		1);
+}
+
 
 void WIIPointExtractor::extract_points(const pt_frame& frame_, pt_preview& preview_frame_, std::vector<vec2>& points)
 {
     const cv::Mat& frame = frame_.as_const<WIIFrame>()->mat;
-	const std::vector<vec2>& pts = frame_.as_const<WIIFrame>()->points;
-	const vec2* ptds = pts.data();
+	struct wii_info& wii = frame_.as<WIIFrame>()->wii;
     cv::Mat& preview_frame = *preview_frame_.as<WIIPreview>();
-	int point_count = points.size();
 
-	auto fun = [&](const vec2& p, const cv::Scalar& color, int thinkness = 1)
-	{
-		static constexpr int len = 9;
+	//create a blank frame
+	cv::Mat blank_frame(preview_frame.cols, preview_frame.rows, CV_8UC3, cv::Scalar(0, 0, 0));
+	//cv::cvtColor(_frame, _frame2, cv::COLOR_BGR2BGRA);
+	cv::resize(blank_frame, preview_frame, cv::Size(preview_frame.cols, preview_frame.rows), 0, 0, cv::INTER_NEAREST);
 
-		cv::Point p2(iround(p[0] * preview_frame.cols + preview_frame.cols / 2),
-			iround(-p[1] * preview_frame.cols + preview_frame.rows / 2));
-
-		cv::line(preview_frame,
-			cv::Point(p2.x - len, p2.y),
-			cv::Point(p2.x + len, p2.y),
-			color,
-			thinkness);
-		cv::line(preview_frame,
-			cv::Point(p2.x, p2.y - len),
-			cv::Point(p2.x, p2.y + len),
-			color,
-			thinkness);
-	};
-	for (unsigned i = 0; i < PointModel::N_POINTS; ++i)
-	{
-		//if (dot_sizes)
-		//	fun(ptds[i], cv::Scalar(0, 255, 0), dot.Size);
-		//else
-			fun(ptds[i], cv::Scalar(0, 255, 0));
-	}
+	_draw_bg(preview_frame, wii);
+	_draw_points(preview_frame, wii, points);
 }
 
